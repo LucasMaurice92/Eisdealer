63b39a0c1ba5745180002edd6c88f2c750b4c46a
Maurice Lester Lucas <32566062+LucasMaurice92@users.noreply.github.com>
2019-05-12 15:28:09 +0200

    Add files via upload

--- L05_Server/Server/Server.js
+++ L05_Server/Server/Server.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Http = require("http");
+const Url = require("url");
 var L05_Server;
 (function (L05_Server) {
     console.log("Starting server");
@@ -18,7 +19,11 @@ var L05_Server;
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
+        let url = Url.parse(_request.url, true);
+        for (let key in url.query)
+            _response.write(key + ":" + url.query[key] + "<br/>");
+        // let jsonString: string = JSON.stringify(url.query);
+        // _response.write(jsonString);
         _response.end();
     }
 })(L05_Server || (L05_Server = {}));
--- L05_Server/Server/Server.ts
+++ L05_Server/Server/Server.ts
@@ -1,4 +1,5 @@
 import * as Http from "http";
+import * as Url from "url";
 
 namespace L05_Server {
 	console.log("Starting server");
@@ -21,7 +22,12 @@ namespace L05_Server {
 		_response.setHeader("content-type", "text/html; charset=utf-8");
 		_response.setHeader("Access-Control-Allow-Origin", "*");
 
-		_response.write(_request.url);
+		let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+		for (let key in url.query)
+			_response.write(key + ":" + url.query[key] + "<br/>");
+
+		// let jsonString: string = JSON.stringify(url.query);
+		// _response.write(jsonString);
 
 		_response.end();
 	}
